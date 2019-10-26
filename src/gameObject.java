f422a2284f30f9b3bc27f212c6019a38638144b1
Abhay Kaushik <abhay.kaushik@mail.utoronto.ca>
2019-10-26 12:33:17 -0400

    Started working on game objects

---
+++ src/gameObject.java
@@ -0,0 +1,17 @@
+
+public abstract class gameObject {
+	
+	private int x;
+	private int y;
+	private boolean isDead;
+	
+	public gameObject(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int[] getPosition() {
+		int[] newPosition = new int[2];
+	}
+	
+}
