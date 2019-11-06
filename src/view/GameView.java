f02be490c15d0a6384b340c9ecd05bdd68879c25
michael <michael.shearer@mail.utoronto.ca>
2019-11-06 11:42:14 -0500

    view fix

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
@@ -17,7 +18,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
-
 import model.GameModel;
 
 /*
@@ -25,7 +25,7 @@ import model.GameModel;
  * Class that creates the game window
  */
 
-public class GameView extends Application {
+public class GameView extends Pane {
 	GameModel gameModel = new GameModel();
 	public Pane root = new Pane();
 
@@ -33,27 +33,15 @@ public class GameView extends Application {
 		this.gameModel = gameModel;
 	}
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		launch(args);
-	}
+	}*/
 
-	/*
-	 * Creates a window of specified height (in this case 300, 200) and initializes
-	 * all game objects as children of the root
-	 * 
-	 * @param primaryStage the main stage (window) where the game will execute
-	 */
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Space Rocks");
+	public Parent start() {
 
 		//add background color
 		BackgroundFill back = new BackgroundFill(Color.BLACK, new CornerRadii(1), new Insets(0.0,0.0,0.0,0.0));
 		this.root.setBackground(new Background(back));
-
-		// root.getChildren().add();
-		// uncomment ^^^ after figuring out the adding procedure for game objects
-		primaryStage.setScene(new Scene(this.root, GameModel.SCREEN_HEIGHT, GameModel.SCREEN_WIDTH));
 		
 		// retrieving polygon from GameModel
 		ArrayList<Polygon> addedPolygon = new ArrayList<Polygon>();
@@ -63,19 +51,25 @@ public class GameView extends Application {
 		for (int counter = 0; counter < addedPolygon.size(); counter++) {
 			this.root.getChildren().add(addedPolygon.get(counter));
 		}
+		this.root.maxHeight(GameModel.SCREEN_HEIGHT);
+		this.root.maxWidth(GameModel.SCREEN_WIDTH);
 		
-		primaryStage.show();
+		//primaryStage.show();
+		return this.root;
 	}
 
 	public void update() {
 		ArrayList<Polygon> addedPolygon = new ArrayList<Polygon>();
 		ArrayList<Polygon> removedPolygon = new ArrayList<Polygon>();
+		
 		addedPolygon = gameModel.getAddedPolygons();
 		removedPolygon = gameModel.getRemovedPolygons();
+		
 		//Iterating through addedPolygon and adding them to screen 
 		for (int counter = 0; counter < addedPolygon.size(); counter++) {
 			this.root.getChildren().add(addedPolygon.get(counter));
 		}
+		
 		//Iterating through removedPolygon and removing them from screen
 		for (int counter = 0; counter < removedPolygon.size(); counter++) {
 			this.root.getChildren().remove(removedPolygon.get(counter));
