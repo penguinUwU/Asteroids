4a2faae9084b5d2c7044eca7d3480c841822066c
masea3439 <mathieuseahlabbe@hotmail.com>
2019-11-06 10:56:37 -0500

    Add screen wrapping

--- src/model/GameObject.java
+++ src/model/GameObject.java
@@ -25,13 +25,30 @@ public abstract class GameObject{
 	 * @return none
 	 * */
 	public void update(float dt) {
-		this.center = this.center.add(this.velX*dt, this.velY*dt);
-		this.body.setTranslateX(this.body.getTranslateX()+this.velX*dt);
-		this.body.setTranslateY(this.body.getTranslateY()+this.velY*dt);
+		this.translate(this.velX*dt, this.velY*dt);
+		this.wrap();
+	}
+	
+	private void wrap() {
+		if (this.center.getX() > GameModel.SCREEN_WIDTH) {
+			this.translate(-GameModel.SCREEN_WIDTH, 0);
+		} else if (this.center.getX() < 0){
+			this.translate(GameModel.SCREEN_WIDTH, 0);
+		} else if (this.center.getY() > GameModel.SCREEN_HEIGHT) {
+			this.translate(0, -GameModel.SCREEN_HEIGHT);
+		} else if (this.center.getY() < 0) {
+			this.translate(0, GameModel.SCREEN_HEIGHT);
+		}
+	}
+	
+	private void translate(double dx, double dy) {
+		this.center = this.center.add(dx, dy);
+		this.body.setTranslateX(this.body.getTranslateX()+dx);
+		this.body.setTranslateY(this.body.getTranslateY()+dy);
 	}
 		
 	/*
-	 * @return the shape of the object.
+	 * @return the polygon representation of the object.
 	 * */
 	public Polygon getBody() {
 		return this.body;
