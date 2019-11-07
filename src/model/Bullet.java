6ed1d08557ce24cdc6fbd53a7381e13038a52035
Abhay Kaushik <abhay.kaushik@mail.utoronto.ca>
2019-11-06 23:38:14 -0500

    Updated the method reponsible for moving the user

---
+++ src/model/Bullet.java
@@ -0,0 +1,13 @@
+package model;
+
+import javafx.scene.paint.Color;
+
+public class Bullet extends GameObject {
+	public Bullet() {
+		this.body.setStroke(Color.WHITE);
+	}
+	
+	public void update(float dt) {
+		
+	}
+}
