bba88bb34773035d8bb752379800cfc15953472d
Abhay Kaushik <abhay.kaushik@mail.utoronto.ca>
2019-11-10 15:17:52 -0500

    Updated update method for bullets kaushi33

--- src/model/Bullet.java
+++ src/model/Bullet.java
@@ -3,11 +3,19 @@ package model;
 import javafx.scene.paint.Color;
 
 public class Bullet extends GameObject {
+	
+	private static final int BULLET_VEL = 150;
+	
 	public Bullet() {
 		this.body.setStroke(Color.WHITE);
 	}
 	
-	public void update(float dt) {
+	public void update(float dt, double rotation, double positionX, double positionY) {
+		
+		this.velX = BULLET_VEL * java.lang.Math.cos(rotation);
+		this.velY = BULLET_VEL * java.lang.Math.sin(rotation);
+		
+		super.update(dt);
 		
 	}
 }
