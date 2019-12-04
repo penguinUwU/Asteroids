1e04e60104be44edf14178c676ddde978f5252e2
Evan Cui <evanflyingpenguin@gmail.com>
2019-12-04 18:54:11 -0500

    fix javadoc comments

--- src/controller/ButtonPressEventHandler.java
+++ src/controller/ButtonPressEventHandler.java
@@ -4,10 +4,15 @@ import controller.GameController;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+/**
+ * Handles JavaFX Button press events for the restart game button.
+ * 
+ * @author Michael Shearer
+ */
 public class ButtonPressEventHandler implements EventHandler<ActionEvent>{
 	GameController controller;
 	
-	/*
+	/**
 	 * Handler constructor
 	 * @param controller: the game that we want to have attached
 	 */
@@ -15,7 +20,7 @@ public class ButtonPressEventHandler implements EventHandler<ActionEvent>{
 		this.controller = controller;
 	}
 	
-	/*
+	/**
 	 * This method gets called upon a button being pressed, then updates the grid by calling observers
 	 * @param event: the event being handled
 	 */
