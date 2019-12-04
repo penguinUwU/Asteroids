8b12a1c12323dd0d56fa1f6c13e7b711245d985d
Evan Cui <evanflyingpenguin@gmail.com>
2019-12-04 01:02:03 -0500

    revise comments of GameController.java

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -12,24 +12,41 @@ import javafx.stage.Stage;
 import model.GameModel;
 import view.GameView;
 
+/**
+ * The Controller in the MVC design pattern.
+ * This has the Model object, the View object, user inputs, and the game loop.
+ * This is the entry point for the users.
+ * When the main function is executed, 
+ * the game objects are initiated and the game loop starts.
+ * 
+ * @author Yichen (Evan) Cui
+ *
+ */
 public class GameController extends Application {
 	
+	// Model and View objects
 	private GameView gameView;
 	private GameModel gameModel;
+	
+	// JavaFX objects
 	private Scene scene;
 	private Stage stage;
 	
+	// HashMap for keys to actions
     private HashMap<KeyCode, String> controls;
+    // HashMap for actions to action states
 	private HashMap<String, Boolean> inputs;
+	
+	// amount of time between each game loop iteration
 	private double pollTime;
+	// the time of when the previous game loop iteration was complete
 	private double previousTime;
 	
 	/**
-	 * resets all game values to its default values
+	 * resets all game objects to its default values
 	 */
 	public void resetGame() {
-		//NEEDS WORK, need to re-attach the scene and gameview
-		// new GameView and GameModel objects
+		// create new GameView and GameModel objects
 		gameView = new GameView();
 		gameModel = new GameModel();
 		gameModel.addObserver(gameView);
@@ -66,57 +83,60 @@ public class GameController extends Application {
 		pollTime = 0;
 		previousTime = 0;
 		
-		
 		// set action of key presses
 		scene.setOnKeyPressed(e -> {
 			KeyCode key = e.getCode();
 			
-			if (controls.containsKey(key)) {
+			// update inputs HashMap with each key press
+			if (controls.containsKey(key))
 				inputs.put(controls.get(key), true);
-			}
 		});
 		
 		// set action of key releases
 		scene.setOnKeyReleased(e -> {
 			KeyCode key = e.getCode();
 			
-			if (controls.containsKey(key)) {
+			// update inputs HashMap with each key release
+			if (controls.containsKey(key))
 				inputs.put(controls.get(key), false);
-			}
 		});
 	}
 	
 	/**
-	 * starts the game
+	 * creates the game loop, and start the game
+	 * @param stage 
 	 */
 	@Override
 	public void start(Stage stage) {
-		// reset all values
 		this.stage = stage;
+		
+		// reset all game values
 		resetGame();
 
-		// create game loop, defaults to 60 fps
+		// create game loop, capped at 60 iterations per second
 		AnimationTimer gameLoop = new AnimationTimer() {
 			@Override
 			public void handle(long currentTime) {
-				//check if previousTime has not been set
-				if (previousTime == 0) {
+				// in the first iteration of the game loop,
+				// the previousTime should be the currentTime
+				if (previousTime == 0)
 					previousTime = ((double)currentTime) / 1000000000;
-				}
+				
+				// calculate the time difference between update iterations
 		        pollTime = ((double)currentTime) / 1000000000 - previousTime;
 		        previousTime = ((double)currentTime) / 1000000000;
 		        
+		        // update GameModel with time difference and input updates
 		        gameModel.update(pollTime, inputs);
-		        if (gameModel.isGameOver()) {
+		        
+		        // when the game is over, the game will restart
+		        if (gameModel.isGameOver())
 		        	resetGame();
-		        }
 			}
 		};
 		
-		// start game loop
+		// start game loop and display the scene
 		gameLoop.start();
-		
-		// show scene
 		stage.show();
 	}
 	
